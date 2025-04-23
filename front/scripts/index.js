c79c6a05eb5d6975c1a72d6541d75401f2a4475a
Joaco Curbelo <joacoc-10@hotmail.com>
2025-04-23 21:33:33 +0200

    Web peliculas avanzada - Clase 2

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,60 +1,54 @@
-console.log(tempData);
 
+const contenedorPeliculas = document.getElementById('contenedor_peliculas');
 
+$.get('https://students-api.up.railway.app/movies', (data) => {
+renderCards(data);
+});
 
-function crearTarjetasPeliculas(pelicula){
+const renderCards = (data) => {
 
-  const divContenedorTarjetas = document.createElement('div');
-  const tituloPeli = document.createElement('h3');
-  const anoPeli = document.createElement('p');
-  const directorPeli = document.createElement('p');
-  const duracionPeli = document.createElement('p');
-  const generoPeli = document.createElement('p');
-  const ratePeli = document.createElement('p');
-  const posterPeli = document.createElement('img');
-  const divContenedorInfo = document.createElement('div'); //
+  data.forEach((pelicula) => {
+    const divContenedorTarjetas = document.createElement('div');
+    divContenedorTarjetas.classList.add('tarjeta_pelis');
+  
+    const tituloPeli = document.createElement('h3');
+    tituloPeli.innerHTML = pelicula.title;
+  
+    const posterPeli = document.createElement('img');
+    posterPeli.src = pelicula.poster;
+  
+    const anoPeli = document.createElement('p');
+    anoPeli.innerHTML = `Año: ${pelicula.year}`;
+  
+    const directorPeli = document.createElement('p');
+    directorPeli.innerHTML = `Director: ${pelicula.director}`;
+  
+    const duracionPeli = document.createElement('p');
+    duracionPeli.innerHTML =`Duración: ${pelicula.duration}`;
+  
+    const generoPeli = document.createElement('p');
+    generoPeli.innerHTML =`Genero: ${pelicula.genre}`;
+  
+    const ratePeli = document.createElement('p');
+    ratePeli.innerHTML = `Puntuacion: ${pelicula.rate}`;
+  
+  
+    const divContenedorInfo = document.createElement('div'); 
+    divContenedorInfo.classList.add('info_pelicula');
+    divContenedorInfo.appendChild(anoPeli);
+    divContenedorInfo.appendChild(directorPeli);
+    divContenedorInfo.appendChild(duracionPeli);
+    divContenedorInfo.appendChild(generoPeli);
+    divContenedorInfo.appendChild(ratePeli);
+  
+    const divTituloInfoContainer = document.createElement('div');
+    divTituloInfoContainer.classList.add('titulo_info_container');
+    divTituloInfoContainer.appendChild(tituloPeli);
+    divTituloInfoContainer.appendChild(divContenedorInfo);
+  
+    divContenedorTarjetas.appendChild(posterPeli);
+    divContenedorTarjetas.appendChild(divTituloInfoContainer);
 
-  tituloPeli.innerHTML = pelicula.title;
-  posterPeli.src = pelicula.poster;
-  directorPeli.innerHTML = `Director: ${pelicula.director}`;
-  anoPeli.innerHTML = `Año: ${pelicula.year}`;
-  duracionPeli.innerHTML =`Duración: ${pelicula.duration}`;
-  generoPeli.innerHTML =`Genero: ${pelicula.genre}`;
-  ratePeli.innerHTML = `Puntuacion: ${pelicula.rate}`;
-
-  divContenedorTarjetas.classList.add('tarjeta_pelis');
-  divContenedorInfo.classList.add('info_pelicula');
- 
- 
-  // tituloPeli.classList.add('info_pelicula');
-  // anoPeli.classList.add('info_pelicula');
-  // directorPeli.classList.add('info_pelicula');
-  // duracionPeli.classList.add('info_pelicula');
-  // generoPeli.classList.add('info_pelicula');
-  // ratePeli.classList.add('info_pelicula');
-  divContenedorInfo.appendChild(anoPeli);
-  divContenedorInfo.appendChild(directorPeli);
-  divContenedorInfo.appendChild(duracionPeli);
-  divContenedorInfo.appendChild(generoPeli);
-  divContenedorInfo.appendChild(ratePeli);
-
-  divContenedorTarjetas.appendChild(tituloPeli);
-  divContenedorTarjetas.appendChild(posterPeli);
-  divContenedorTarjetas.appendChild(divContenedorInfo);
-  // divContenedorTarjetas.appendChild(directorPeli);
-  // divContenedorTarjetas.appendChild(generoPeli);
-  // divContenedorTarjetas.appendChild(anoPeli);
-  // divContenedorTarjetas.appendChild(duracionPeli);
-  // divContenedorTarjetas.appendChild(ratePeli);
-
-  return divContenedorTarjetas 
-  }
-
-  const contenedorPeliculas = document.getElementById('contenedor_peliculas');
-
-
-  const tarjetasDOM = tempData.map(pelicula => crearTarjetasPeliculas(pelicula));
-  tarjetasDOM.forEach(tarjeta => {
-  contenedorPeliculas.appendChild(tarjeta);
-  })
-
+    contenedorPeliculas.appendChild(divContenedorTarjetas);
+  });  
+}
