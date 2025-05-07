7b3ca630a9d3398bd7e390d87605f7305a757eda
Joaco Curbelo <joacoc-10@hotmail.com>
2025-05-07 21:35:51 +0200

    Proyecto para entregar

--- front/scripts/formControl.js
+++ front/scripts/formControl.js
@@ -1,46 +1,47 @@
-const { postMovie } = require("./services")
+const { postMovie } = require("./services");
 
 const formValidation = () => {
-  const forms = document.querySelectorAll('.needs-validation')
+  const forms = document.querySelectorAll(".needs-validation");
 
-  Array.from(forms).forEach(form => {
+  Array.from(forms).forEach((form) => {
     form.addEventListener(
-      'submit',event => {
-        event.preventDefault()
-          if(!form.checkVisibility()) {
-            event.stopPropagation()
-          }
-          form.classList.add('was-validated')
-          const selectedGenres = Array.from(
-            document.querySelectorAll('input[name="genre"]:checked')
-          ).map(checkbox => checkbox.value)
+      "submit",
+      (event) => {
+        event.preventDefault();
+        if (!form.checkVisibility()) {
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+        const selectedGenres = Array.from(
+          document.querySelectorAll('input[name="genre"]:checked')
+        ).map((checkbox) => checkbox.value);
 
-          const genreError = document.getElementById('genre-error');
+        const genreError = document.getElementById("genre-error");
 
-          if (selectedGenres.length === 0) {
-            genreError.textContent = 'Por favor, selecciona al menos un género.';
-            genreError.classList.add('text-danger'); 
-            hasError = true; 
-          } else {
-            genreError.textContent = ''; 
-            genreError.classList.remove('text-danger');
-          }
+        if (selectedGenres.length === 0) {
+          genreError.textContent = "Por favor, selecciona al menos un género.";
+          genreError.classList.add("text-danger");
+          hasError = true;
+        } else {
+          genreError.textContent = "";
+          genreError.classList.remove("text-danger");
+        }
 
-          const newMovie = {
-              title: document.querySelector('#tituloId').value,
-              rate: document.querySelector('#valoracionId').value,
-              duration: document.querySelector('#duracionId').value,
-              poster: document.querySelector('#posterId').value,
-              director: document.querySelector('#directorId').value,
-              year: document.querySelector('#anoId').value,
-              genre: selectedGenres,
-          }
-          if(!hasError){
-            postMovie(newMovie)
-          }
+        const newMovie = {
+          title: document.querySelector("#tituloId").value,
+          rate: document.querySelector("#valoracionId").value,
+          duration: document.querySelector("#duracionId").value,
+          poster: document.querySelector("#posterId").value,
+          director: document.querySelector("#directorId").value,
+          year: document.querySelector("#anoId").value,
+          genre: selectedGenres,
+        };
+        if (!hasError) {
+          postMovie(newMovie);
+        }
       },
       false
-    )
-  })
-}
-module.exports = {formValidation}
+    );
+  });
+};
+module.exports = { formValidation };
