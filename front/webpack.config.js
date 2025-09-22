0fb8e677c6d36efe39031c19205ffd81ee123a46
Joaco Curbelo <joacoc-10@hotmail.com>
2025-09-22 20:45:07 +0200

    Cambios en ruta src de index y crear peli

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -3,22 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './scripts/index.js',
+  entry: {
+    main: './scripts/index.js',
+    crearPelicula: './scripts/formControl.js',
+  },
 
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
+      filename: 'index.html',
+      chunks: ['main'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './pages/crearPelicula.html',
+      filename: 'pages/crearPelicula.html',
+      chunks: ['crearPelicula'],
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'styles', to: 'styles' }, 
-        { from: 'pages', to: 'pages' },
-        { from: 'assets', to: 'assets' }, 
+        { from: 'styles', to: 'styles' },
+        { from: 'pages/about.html', to: 'pages/about.html' },
+        { from: 'pages/contact.html', to: 'pages/contact.html' },
+        { from: 'assets', to: 'assets' },
       ],
     }),
   ]
