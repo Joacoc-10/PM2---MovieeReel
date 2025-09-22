426da18f4729f2899636e707985de37f31767b32
Joaco Curbelo <joacoc-10@hotmail.com>
2025-09-22 20:12:27 +0200

    Subiendo resto de paginas y css

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './scripts/index.js',
@@ -12,6 +13,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html'
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'styles', to: 'styles' }, 
+        { from: 'pages', to: 'pages' },  
+      ],
+    }),
   ]
 };
