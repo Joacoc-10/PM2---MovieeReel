03e77b2459b56b797de980b0bb22853f483a6105
Joaco Curbelo <joacoc-10@hotmail.com>
2025-09-22 21:13:36 +0200

    Cambio en index del back

--- back/index.js
+++ back/index.js
@@ -3,10 +3,13 @@
 const app = require('./src/server');
 const dbCon = require('./src/config/dbCon');
 
+
+const port = process.env.PORT || 3000;
+
 dbCon()
 .then((res) => {
-    app.listen(3000, () => {
-      console.log('Servidor encontrado en el puerto 3000!');
+    app.listen(port, () => {
+      console.log(`Servidor encontrado en el puerto ${port}!`);
     })
   })
   .catch((err) => {
